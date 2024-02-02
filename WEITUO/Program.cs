3d0b5762fe0636a9aaad25300235e2c2902e884a
Liuzhen <929998390@qq.com>
2024-02-02 14:55:07 +0800

    委托的知识

---
+++ WEITUO/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEITUO
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            dailiren dailiren = new dailiren();
+            dailiren.TestDelegate();
+            dailiren.Test1();
+
+        }
+
+        
+       
+     
+    }
+
+}
---
+++ WEITUO/被代理人.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEITUO
+{
+    //委托（delegate)有许多函数委托和时间在，.Net Framework中的应用非常广泛指针不具备的优点，委托（delegate)既可以引用静态类型，又可以引用非静态成员函数。delegate是面向对象，类型安全，可靠的受控（managed)对象。也就是说，runtime能够摆正delegate指向的一个有效的方法。你无需担心delegate会指向无效地址或越界地址。
+    //方法的签名：是参数列表和返回值的统称
+    //委托的签名要与方法的签名保持一致
+    public class dailiren
+    {
+
+        public void beidailiren(string houseName, string address)
+        {
+
+        }
+        //public delegate 被代理人(string houseName, string address);
+
+        //委托列表
+        //<访问修饰符> delegate 返回值  委托名称(<参数列表>)
+        //例如：public delegate 被代理人(string houseName, string address);
+        public delegate void SpeakDel();//无参无返回值的委托
+        /*声明委托的使用方法*/
+
+
+      void Speak()
+        {
+            Console.WriteLine("我是翻译官");
+        }
+       public void SpeakFun()
+        {
+            Console.WriteLine("我是翻译官");
+        }
+        /*委托即将要进行的行为*/
+        public void TestDelegate()
+        {
+            //创建一个委托对象，并使用代理对象执行具体的代理方
+           
+            SpeakDel speakdel = new SpeakDel(Speak);//委托使用方法名当做参数来使用，在一个方法里调用另一个方法的一种手段叫做委托。
+            speakdel();//使用委托声明的变量直接当做无参方法使用
+            //
+            SpeakDel spdel = SpeakFun;//也可以使用第二种写法直接读取方法。
+            
+            spdel.Invoke();//使用Invoke方法调用
+
+
+
+            SpeakFun(); //普通的方法的调用
+
+            //委托的应用场景
+            //1.可以当做方法的回调
+            //2.使用应用在多线程
+
+            
+
+            
+
+
+        }
+        //委托是什么；委托是一个密封类型
+        public void Test1()
+        {
+            //第一种方法。反射可以看清一个对象的本质；
+           var weituo= typeof(SpeakDel);
+            Console.WriteLine(weituo.IsClass);//是否为类
+            Console.WriteLine(weituo.IsSealed);//是否为密封类
+            var fulei=  weituo.BaseType;//获取它的父类
+            Console.WriteLine(fulei.FullName);
+            
+
+
+
+        }
+
+       //多播委托；
+       public void TsetMultDeletes()
+        {
+            SpeakDel del = SpeakFun;
+        }
+
+    }
+}
+
